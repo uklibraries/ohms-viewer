500ec29e0b84166f08d7b736c36a5c0ddeba19e1
root <root@centos.artifex.hq.local>
2014-05-02 18:29:46 -0400

    Final YouTube commit.

--- tmpl/player_other.tmpl.php
+++ tmpl/player_other.tmpl.php
@@ -1,23 +1,20 @@
 <?php
 
 echo "<style>
-              #transcript-panel { height:350px; }
-              #index-panel { height:350px; }
-              #searchbox-panel { height:350px; }
+              #transcript-panel { height:550px; }
+              #index-panel { height:550px; }
+              #searchbox-panel { height:550px; }
               #search-results { height:177px; }
-              #audio-panel { height: 270px; width:670px; }
-              #header {height: 415px; }
-              #main {height: 350px; }
+              #audio-panel { height: 270px; }
+              #header {height: " . $headerheight . "; }
+              #main {height: 550px; }
+			  div.centered { margin-left: 35px; }
             </style>";
 
 $filepath = $cacheFile->media_url;
 echo '<div class="centered">';
 if($cacheFile->clip_format=='audio' || $cacheFile->clip_format=='audiotrans') {
 	if(strpos($filepath,'http://') !== false || strpos($filepath,'https://') !== false) {
-		echo "<style>
-        		      #header {height: 200px; }
-        		      #audio-panel { height: 100px; width:670px; }              
-			  </style>";
 		echo '<a href="' . $filepath .'" id="subjectPlayer"></a>';
 	} else {
 		echo '<a href="http://' . $config['fileserver'] . $cacheFile->file_name .'" id="subjectPlayer"></a>';
--- tmpl/player_youtube.tmpl.php
+++ tmpl/player_youtube.tmpl.php
@@ -9,13 +9,15 @@
 		  <div class="video-spacer"></div>
 
 		  <style>
-		    #transcript-panel { height:350px; }
-		    #index-panel { height:350px; }
-		    #searchbox-panel { height:350px; }
+		    #transcript-panel { height:550px; }
+		    #index-panel { height:550px; }
+		    #searchbox-panel { height:544px; }
 		    #search-results { height:177px; }
-		    #audio-panel { height: 270px; width:670px; }
+		    #audio-panel { height: 270px; }
 		    #header {height: 415px; }
-		    #main {height: 350px; }
+			#headervid {height: 380px; }
+		    #main {height: 550px; }
+			#youtubePlayer {margin-left: 50px;}
 		  </style>
 			<script type="text/javascript">
 				var tag = document.createElement('script');
