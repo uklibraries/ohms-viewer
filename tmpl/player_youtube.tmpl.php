daa4d461adf9c67ed39dd983e7bdbc2d3ce8ae52
Michael Slone <m.slone@gmail.com>
2016-06-20 15:30:30 -0400

    Revert "Reapply CSS changes."
    This reverts commit 93efddf3382425e3bfd42b2a2a592874a1ef0f83.

--- tmpl/player_youtube.tmpl.php
+++ tmpl/player_youtube.tmpl.php
@@ -1,52 +1,59 @@
 <?php
-//Set style values for Youtube player and page based on file format
-if ($cacheFile->clip_format == 'audio') {
-    $height = "180";
-    $width  =  "450";
-    $styleheight = "300";
-} else {
-    $height = "270";
-    $width  =  "500";
-    $styleheight = "415";
-}
+		$player_id = $cacheFile->player_id;
+		$publisher_id = $cacheFile->account_id;
+		$youtubeId = str_replace('http://youtu.be/', '', $cacheFile->media_url);
 
-        $player_id = $cacheFile->player_id;
-        $publisher_id = $cacheFile->account_id;
-        $youtubeId = str_replace('http://youtu.be/', '', $cacheFile->media_url);
-        $extraScript = '';
-        if(isset($_GET['time']) && is_numeric($_GET['time']))
-        {
-            $extraScript = 'event.target.seekTo(' . (int)$_GET['time'] . ');';
-        }
+		$extraScript = '';
+		if(isset($_GET['time']) && is_numeric($_GET['time']))
+		{
+			$extraScript = 'event.target.seekTo(' . (int)$_GET['time'] . ');';
+		}
+
 echo <<<YOUTUBE
-            <div id="youtubePlayer" style="width: 500px; height: {$height}px;"></div>
-            
+			<div id="youtubePlayer"></div>
+			
+		  <div class="video-spacer"></div>
 
-            <script type="text/javascript">
-                var tag = document.createElement('script');
-                tag.src = "https://www.youtube.com/iframe_api";
-                var firstScriptTag = document.getElementsByTagName('script')[0];
-                firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-                
-                var player;
-                var setTime = 0;
-                function onYouTubeIframeAPIReady() {
-                    player = new YT.Player('youtubePlayer', {
-                        height: '270',
-                        width: '480',
-                        videoId: '{$youtubeId}',
-                        startAt: setTime,
-                        events: {
-                            onReady: onPlayerReady
-                        }
-                    });
-                    
-                    function onPlayerReady(event)
-                    {
-                        event.target.playVideo();
-                        {$extraScript}
-                    }
-                }
-            </script>
+		  <style>
+		    #transcript-panel { height:550px; }
+		    #index-panel { height:550px; }
+		    #searchbox-panel { height:544px; }
+		    #search-results { height:177px; }
+		    #audio-panel { height: 270px; }
+		    #header {height: 415px; }
+			#headervid {height: auto; padding-bottom: 1px; }
+		    #main {height: 550px; }
+			#youtubePlayer {margin-left: 50px;}
+			.video-spacer {height: 0px; }
+		  </style>
+			<script type="text/javascript">
+				var tag = document.createElement('script');
+
+				tag.src = "https://www.youtube.com/iframe_api";
+				var firstScriptTag = document.getElementsByTagName('script')[0];
+				firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+				
+				var player;
+				var setTime = 0;
+
+				function onYouTubeIframeAPIReady() {
+					player = new YT.Player('youtubePlayer', {
+						height: '270',
+						width: '480',
+						videoId: '{$youtubeId}',
+						startAt: setTime,
+						events: {
+							onReady: onPlayerReady
+						}
+					});
+					
+					function onPlayerReady(event)
+					{
+						event.target.playVideo();
+						{$extraScript}
+					}
+				}
+			</script>
 YOUTUBE;
+
 ?>
