adcc0897bdd2d1b94c780042115ae6fe24346b8f
Ali Shaikh <Ali-Shaikh@users.noreply.github.com>
2018-07-03 16:13:06 +0500

    Ohms 88 (#19)
    * OHMS-88 Fixed
    
    * OHMS-88 - Use quotation marks to put text together in a search string.

--- app/Ohms/Transcript.php
+++ app/Ohms/Transcript.php
@@ -257,6 +257,11 @@ FOOTNOTE;
         return $q_kw;
     }
 
+    private function stripQuotes($text) {
+        $unquoted = preg_replace('/^(\'(.*)\'|"(.*)")$/', '$2$3', $text);
+        return $unquoted;
+    }
+
     public function keywordSearch($keyword)
     {
         # quote kw for later
@@ -285,6 +290,7 @@ FOOTNOTE;
     public function indexSearch($keyword, $translate)
     {
         if (!empty($keyword)) {
+            $keyword = $q_kw = $this->stripQuotes($keyword);
             $q_kw = $this->quoteWords($keyword);
             $metadata = array(
                 'keyword' => $q_kw,
@@ -300,13 +306,18 @@ FOOTNOTE;
                 $timePoint = floor($time / 60) . ':' . str_pad($time % 60, 2, '0', STR_PAD_LEFT);
                 $gps = $point->gps;
                 $hyperlink = $point->hyperlink;
+                //                OHMS-88 Fix
+                $partial_transcript = $point->partial_transcript;
 
                 if (preg_match("/{$this->fixAccents($keyword)}/imsU", $this->fixAccents($synopsis)) > 0
                 || preg_match("/{$this->fixAccents($keyword)}/ismU", $this->fixAccents($title)) > 0
                 || preg_match("/{$this->fixAccents($keyword)}/ismU", $this->fixAccents($keywords)) > 0
                 || preg_match("/{$this->fixAccents($keyword)}/ismU", $this->fixAccents($subjects)) > 0
                 || preg_match("/{$this->fixAccents($keyword)}/ismu", $this->fixAccents($gps)) > 0
-                || preg_match("/{$this->fixAccents($keyword)}/ismu", $this->fixAccents($hyperlink)) > 0) {
+                || preg_match("/{$this->fixAccents($keyword)}/ismu", $this->fixAccents($hyperlink)) > 0
+                || preg_match("/{$this->fixAccents($keyword)}/ismu", $this->fixAccents($partial_transcript)) > 0)
+                    //                OHMS-88 Fix ----> END
+                {
                     $metadata['matches'][] = array(
                         'time' => (string)$time,
                         'shortline' => $timePoint . ' - ' . $this->quoteChange($title),
