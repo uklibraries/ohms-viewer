1373b20d6bc803d61fb49fc0a5fdff2937190b82
benk <benkyriakou@users.noreply.github.com>
2018-07-21 21:00:33 +0100

    Added processing of $_REQUEST['cachefile'] with basename().
    This prevents values like '../../path/to/file.xml' being passed on, which can
    be resolved outside of the configured directory by file_get_contents().

--- index.php
+++ index.php
@@ -12,8 +12,9 @@ if (!isset($_GET['translate'])) {
 if (isset($_REQUEST['cachefile'])) {
     $kw = (isset($_REQUEST['kw'])) ? $_REQUEST['kw'] : null;
     $action = (isset($_REQUEST['action'])) ? $_REQUEST['action'] : null;
-    $vController = new ViewerController($_REQUEST['cachefile']);
-    $vController->route($action, $kw, $_REQUEST['cachefile']);
+    $cachefile = basename($_REQUEST['cachefile']);
+    $vController = new ViewerController($cachefile);
+    $vController->route($action, $kw, $cachefile);
 } else {
     header('HTTP/1.0 404 Not Found');
     //echo 'Error no action to take.';
