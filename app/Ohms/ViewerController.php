ffadb3151b81e11a981713b9042bcf3b211eb5c1
Sunny Walker <swalker@hawaii.edu>
2021-11-04 13:24:15 -1000

    reformat; fix transcript call; remove unused parameter in route

--- app/Ohms/ViewerController.php
+++ app/Ohms/ViewerController.php
@@ -1,62 +1,89 @@
-<?php namespace Ohms;
+<?php
+namespace Ohms;
 
-use Ohms\Interview;
-use Ohms\CustomPdf;
+use Exception;
+use Ohms\Interview\AbstractInterview;
 
+/**
+ * View Controller
+ *
+ * @copyright Copyright &copy; 2012 Louie B. Nunn Center, University of Kentucky
+ * @link      http://www.uky.edu
+ * @license   https://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ */
 class ViewerController
 {
-    private $interview;
-    private $interviewName;
-    private $tmpDir;
-    private $config;
-    public function __construct($interviewName)
+    /**
+     * @var ?AbstractInterview
+     */
+    protected ?AbstractInterview $interview;
+
+    /**
+     * @var string|null
+     */
+    protected ?string $interviewName;
+
+    /**
+     * @var array|false
+     */
+    protected array $config;
+
+    /**
+     * @param ?string $interviewName
+     * @throws Exception
+     */
+    public function __construct(?string $interviewName)
     {
-        $this->config = parse_ini_file("config/config.ini", true);
-        $this->interview = Interview::getInstance($this->config, $this->config['tmpDir'], $interviewName);
+        $this->config        = parse_ini_file("config/config.ini", true);
+        $this->interview     = Interview::getInstance($this->config, $this->config['tmpDir'], $interviewName);
         $this->interviewName = $interviewName;
     }
 
-    public function route($action, $kw, $interviewName)
+    /**
+     * @param ?string $action
+     * @param ?string $kw
+     */
+    public function route(?string $action, ?string $kw)
     {
-        switch($action) {
+        switch ($action) {
             case 'pdf':
-                CustomPdf::__prepare($this->interview,$this->config);
+                CustomPdf::__prepare($this->interview, $this->config);
                 exit();
             break;
             case 'metadata':
                 header('Content-type: application/json');
                 echo $this->interview->toJSON();
                 exit();
-                break;
+            break;
             case 'xml':
                 header('Content-type: application/xml');
                 echo $this->interview->toXML();
                 exit();
-                break;
+            break;
             case 'transcript':
-                echo $this->interview->getTranscript();
-                break;
+                echo $this->interview->Transcript->getTranscript();
+            break;
             case 'search':
                 if (isset($kw)) {
                     echo $this->interview->Transcript->keywordSearch($kw);
                 }
                 exit();
-                break;
+            break;
             case 'index':
                 if (isset($kw)) {
                     $translate = ($_GET['translate'] == '1' ? 1 : 0);
                     echo $this->interview->Transcript->indexSearch($kw, $translate);
                 }
                 exit();
-                break;
+            break;
             case 'all':
-                break;
+            break;
             default:
-                $interview = $this->interview;
+                $interview     = $this->interview;
                 $interviewName = $this->interviewName;
-                $config = $this->config;
+                $config        = $this->config;
                 include_once 'tmpl/viewer.tmpl.php';
-                break;
+            break;
         }
     }
 }
