361e0a6f09293761cecb48dfe0e4d9f323407058
Nouman Tayyab <noumantayyab@gmail.com>
2024-03-06 12:13:45 +0500

    fixed the vimeo embed code display issue

--- tmpl/player_vimeo.tmpl.php
+++ tmpl/player_vimeo.tmpl.php
@@ -2,11 +2,12 @@
 
 $clipid = $interview->clip_id;
 if ($interview->kembed == "" && $interview->media_url != "") {
-    $height = ($interview->clip_format == 'audio' ? 95 : 279);
+    $height = ($interview->clip_format == 'audio' ? 95 : 350);
     $video_id = str_replace('https://vimeo.com/', '', str_replace('http://vimeo.com/', '', $interview->media_url));
     $embedcode = '<iframe id="vimeo_widget" src="https://player.vimeo.com/video/' . $video_id . '?color=ffffff&badge=0&portrait=false&title=false&byline=false" width="100%" maxwidth="100%" height="' . $height . '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
 } elseif ($interview->kembed != "") {
-        $embedcode = preg_replace('/(width|height)=["\']\d*["\']\s?/', "", $interview->kembed);
+        $embedcode = preg_replace('/\<[\/]{0,1}div[^\>]*\>/i', '', $interview->kembed);
+        $embedcode = preg_replace('/(width|height)=["\']\d*["\']\s?/', "", $embedcode);
         $embedcode = str_replace('<iframe ', '<iframe title="Video Player" id="vimeo_widget"', $embedcode);
 }
 
@@ -17,10 +18,10 @@ if (isset($_GET['time']) && is_numeric($_GET['time'])) {
     $playScript = '';
     $extraScript = '';
 }
-$height = ($interview->clip_format == 'audio' ? 95 : 279);
+$height = ($interview->clip_format == 'audio' ? 95 : 350);
 
 echo <<<VIMEO
-<div class="video embed-responsive embed-responsive-16by9" style="width: 500px; height: {$height}px;margin-left: auto; margin-right: auto;">
+<div class="video embed-responsive embed-responsive-16by9" style="width: auto; height: {$height}px;margin-left: auto; margin-right: auto;">
   <p>&nbsp;</p>
   {$embedcode}
   <script src="https://player.vimeo.com/api/player.js"></script>
