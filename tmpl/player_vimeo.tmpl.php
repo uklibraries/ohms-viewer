b5316b19b5b8226ce60f451ecd7359200fa8f146
Nouman Tayyab <noumantayyab@gmail.com>
2024-08-15 15:29:52 +0500

    fixed the vimeo embed privacy loading issue

--- tmpl/player_vimeo.tmpl.php
+++ tmpl/player_vimeo.tmpl.php
@@ -4,11 +4,11 @@ $clipid = $interview->clip_id;
 if ($interview->kembed == "" && $interview->media_url != "") {
     $height = ($interview->clip_format == 'audio' ? 95 : 350);
     $video_id = str_replace('https://vimeo.com/', '', str_replace('http://vimeo.com/', '', $interview->media_url));
-    $embedcode = '<iframe id="vimeo_widget" src="https://player.vimeo.com/video/' . $video_id . '?color=ffffff&badge=0&portrait=false&title=false&byline=false" width="100%" maxwidth="100%" height="' . $height . '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+    $embedcode = '<iframe referrerpolicy="strict-origin" id="vimeo_widget" src="https://player.vimeo.com/video/' . $video_id . '?color=ffffff&badge=0&portrait=false&title=false&byline=false" width="100%" maxwidth="100%" height="' . $height . '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
 } elseif ($interview->kembed != "") {
         $embedcode = preg_replace('/\<[\/]{0,1}div[^\>]*\>/i', '', $interview->kembed);
         $embedcode = preg_replace('/(width|height)=["\']\d*["\']\s?/', "", $embedcode);
-        $embedcode = str_replace('<iframe ', '<iframe title="Video Player" id="vimeo_widget"', $embedcode);
+        $embedcode = str_replace('<iframe ', '<iframe referrerpolicy="strict-origin" title="Video Player" id="vimeo_widget"', $embedcode);
 }
 
 if (isset($_GET['time']) && is_numeric($_GET['time'])) {
@@ -21,10 +21,10 @@ if (isset($_GET['time']) && is_numeric($_GET['time'])) {
 $height = ($interview->clip_format == 'audio' ? 95 : 350);
 
 echo <<<VIMEO
-<div class="video embed-responsive embed-responsive-16by9" style="position:relative;width: auto; height: {$height}px;margin-left: auto; margin-right: auto;">
+<script src="https://player.vimeo.com/api/player.js"></script>
+    <div class="video embed-responsive embed-responsive-16by9" style="position:relative;width: auto; height: {$height}px;margin-left: auto; margin-right: auto;">
   <p>&nbsp;</p>
   {$embedcode}
-  <script src="https://player.vimeo.com/api/player.js"></script>
   <script>
 var widget = null;
 jQuery(document).ready(function () {
