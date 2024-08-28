b6d497f9dc0b1b84c02f8cdb5190756929c5a09c
Nouman Tayyab <noumantayyab@gmail.com>
2024-08-28 06:35:19 +0500

    fixes for aviary player not working in somecases

--- idebug.php
+++ idebug.php
@@ -9,6 +9,8 @@
  */
 use Ohms\ViewerController;
 
+ini_set('display_errors',1);
+error_reporting(E_ALL);
 require_once 'app/init.php';
 
 session_start();
--- tmpl/player_other.tmpl.php
+++ tmpl/player_other.tmpl.php
@@ -10,7 +10,11 @@ if (strpos($filepath, 'http://') !== false || strpos($filepath, 'https://') !== 
 
 $validClipFormats = array('audio', 'audiotrans', 'video');
 $clipFormat = $interview->clip_format;
-$mediaFormat = (strtolower($interview->clipsource) == "aviary")? $interview->aviaryMediaFormat :substr($linkToMedia, -3, 3);
+if (strtolower($interview->clipsource) == "aviary")
+    $mediaFormat = $clipFormat == 'video' ? 'm4v' : 'mp3';
+else
+   $mediaFormat =  substr($linkToMedia, -3, 3);
+
 
 $class = 'jp-video jp-video-270p';
 $customWidth = '';
